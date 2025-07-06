1ca60082eee691517848c83ac6f67197b922c71b
mohamed amine <amineprograming@gmail.com>
2025-07-06 11:31:48 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cardocuments.app',
+  appName: 'car-document-manager',
+  webDir: 'www'
+};
+
+export default config;
